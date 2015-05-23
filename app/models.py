7f2b80c07090f11d9c0ee3fa5dd6b1b123c9983e
Chris Lindgren <chris.a.lindgren@gmail.com>
2015-05-23 14:27:41 -0500

    reformatted models.py

--- app/models.py
+++ app/models.py
@@ -58,11 +58,8 @@ class User(UserMixin, db.Model):
 	mid_name = db.Column(db.String(64))
 	fam_name = db.Column(db.String(64))
 	fullname = column_property(given_name + ' ' + mid_name + ' ' + fam_name)
-	apa_beg_name = column_property(fam_name + ', ' + given_name[0].capitalize() \
-																				+ '.' + mid_name[0].capitalize() + '.')
-	apa_end_name = column_property(given_name[0].capitalize() + '.' \
-																				+ mid_name[0].capitalize() + '. ' \
-																				+ fam_name)
+	apa_beg_name = column_property(fam_name + ', ' + given_name[0].capitalize() + '.' + mid_name[0].capitalize() + '.')
+	apa_end_name = column_property(given_name[0].capitalize() + '.' + mid_name[0].capitalize() + '. ' + fam_name)
 
 	location = db.Column(db.String(64))
 	about_me = db.Column(db.Text())
